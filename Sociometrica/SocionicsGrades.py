6c811ed3d1b46ae76364fe5b5a48368cb7a3d33d
Xyzzyall <xyzzyall@gmail.com>
2019-12-09 23:36:31 +0300

    i am so tired of this... WIP

--- Sociometrica/SocionicsGrades.py
+++ Sociometrica/SocionicsGrades.py
@@ -21,6 +21,9 @@ class Grades:
         n = len(self.__h__)
         k = np.empty(n)
         for i in range(n):
+            #TODO: спросить куренкова, почему это не работает
+            #k[i] = np.trace(np.matmul(relation.get(self.__refs__[i]).__data__, relation.__data__.transpose()))
+            #print(k[i])
             comp = relation.__data__*relation.get(self.__refs__[i]).__data__
             if np.min(sum(comp)) < COMP_ERROR:
                 k[i] = 0
@@ -45,6 +48,64 @@ class Grades:
         return res
 
 
+def balance(M: np.ndarray, u: bool):
+    def define_s_row(row, n):
+        res = []
+        for i in range(n):
+            if row[i] > 0:
+                res.append(i)
+        return res
 
+    def find_index_of_max_len(array):
+        i = 0
+        res = []
+        max_len = 0
+        max_index = 0
+        for elem in array:
+            if len(elem) > max_len:
+                res = elem
+                max_index = i
+            i += 1
+        return max_index, res
+
+    def is_subset(a, b):
+        for a_elem in a:
+            if a_elem not in b:
+                return False
+        return True
+
+    def g_function(s_rows, s_i_star):
+        s_l = []
+        s_l_rev = []
+        for l in range(len(s_rows)):
+            if is_subset(s_rows[l], s_i_star):
+                s_l.append(l)
+            else:
+                s_l_rev.append(l)
+        return s_l, s_l_rev
+
+    D = M.copy()
+    n = len(D)
+    if u:
+        for i in range(n):
+            D[i, i] = 1
+    for i in range(n):
+        for j in range(n):
+            D[i, j] = D[i, j] if D[i, j] > 0 else 0
+
+    DD = D.copy()
+    res = []
+    while True:
+        s_rows = [define_s_row(row, n) for row in DD]
+        max_i, s_i_star = find_index_of_max_len(s_rows)
+        s_l, s_l_rev = g_function(s_rows, s_i_star)
+        DD = DD[s_l_rev, :]
+        res.append(s_l)
+        if len(DD) == 0:
+            flat_res = []
+            for array in res:
+                for elem in array:
+                    flat_res.append(elem)
+            return flat_res
 
 
--- main.py
+++ main.py
@@ -5,12 +5,34 @@ import Sociometrica as SM
 
 s = SM.Sociometrica([str(i+1) for i in range(16)], Soc.Psychotype.get_names())
 grades = SM.Grades({
-    'TO': 0, 'DU': 1, 'AK': 1, 'ZE': 0,
-    'R-': -1, 'Z-': -1, 'MI': -1, 'DE': 0,
-    'SE': 0, 'PO': 1, 'KT': 0, 'KF': 0,
-    'R+': 1, 'Z+': 1, 'PD': 0, 'RO': 0
+    'TO': 0, 'DU': 1, 'AK': 1, 'ZE': 1,
+    'R-': -1, 'Z-': 0, 'MI': 0, 'DE': 0,
+    'SE': 1, 'PO': 1, 'KT': -1, 'KF': 0,
+    'R+': 1, 'Z+': 1, 'PD': 0, 'RO': -1
 })
 
 br = SM.BinaryRelations.from_sociotypes(s, grades)
 print(br)
 print(grades)
+
+balanced = SM.SocionicsGrades.balance(br.__data__, True)
+
+print(balanced)
+n = len(br.__data__)
+dat = br.__data__
+print(dat)
+dat_balanced = dat[balanced].transpose()[balanced].transpose()
+
+dat_numerated = np.zeros((n+1, n+1))
+for i in range(n+1):
+    for j in range(n+1):
+        if i == 0 and j == 0:
+            dat_numerated[i, j] = 0
+        elif i == 0:
+            dat_numerated[i, j] = balanced[j-1]+1
+        elif j == 0:
+            dat_numerated[i, j] = balanced[i-1]+1
+        else:
+            dat_numerated[i, j] = dat_balanced[i-1, j-1]
+
+print(dat_numerated)
