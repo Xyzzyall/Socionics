31a7b4331d3fe378df2448b36deddb6e90f78e81
Xyzzyall <xyzzyall@gmail.com>
2019-12-13 20:56:07 +0300

    WIP

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ grades = SM.Grades({
     'TO': 0, 'DU': 1, 'AK': 1, 'ZE': 1,
     'R-': -1, 'Z-': 0, 'MI': 0, 'DE': 0,
     'SE': 1, 'PO': 1, 'KT': -1, 'KF': 0,
-    'R+': 1, 'Z+': 1, 'PD': 0, 'RO': -1
+    'R+': 1, 'Z+': 1, 'PD': 1, 'RO': -1
 })
 
 br = SM.BinaryRelations.from_sociotypes(s, grades)
@@ -21,7 +21,7 @@ print(balanced)
 n = len(br.__data__)
 dat = br.__data__
 print(dat)
-dat_balanced = dat[balanced].transpose()[balanced].transpose()
+dat_balanced = dat[np.ix_(balanced, balanced)]#dat[balanced].transpose()[balanced].transpose()
 
 dat_numerated = np.zeros((n+1, n+1))
 for i in range(n+1):
