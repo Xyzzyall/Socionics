006d241e5125ded34e9c4146645f7c3a00e562cf
xyzzy <xyzzyall@outlook.com>
2019-12-26 19:45:10 +0300

    Balance function completed

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import Sociometrica as SM
 
 s = SM.Sociometrica([str(i+1) for i in range(16)], Soc.Psychotype.get_names())
 grades = SM.Grades({
-    'TO': 1, 'DU': 1, 'AK': 1, 'ZE': 1,
-    'R-': -1, 'Z-': 0, 'MI': 1, 'DE': 0,
+    'TO': 0, 'DU': 1, 'AK': 1, 'ZE': 1,
+    'R-': 1, 'Z-': 1, 'MI': 1, 'DE': 0,
     'SE': 0, 'PO': 0, 'KT': 0, 'KF': 0,
-    'R+': 0, 'Z+': 0, 'PD': 0, 'RO': 0
+    'R+': 0, 'Z+': 0, 'PD': 0, 'RO': -1
 })
 
 br = SM.BinaryRelations.from_sociotypes(s, grades)
@@ -26,11 +26,14 @@ test = np.array([
     [-1, -1,  0,  0,  0, -1,  1,  0, -1, -1],
     [1,  0,  1,  1,  1,  0,  0,  0,  0,  0],
     [1,  1,  1,  1,  0,  1,  0, -1,  0,  0]
-])
+]) + np.eye(10,10)
 
-balanced = SM.SocionicsGrades.balance(test, u=True)
+data = br.__data__
+balanced = SM.balance(data, u=False)
+dat_balanced = data[np.ix_(balanced, balanced)]
 print(balanced)
-print(test[np.ix_(balanced, balanced)])
+print(dat_balanced)
+print(SM.check_blocks(dat_balanced))
 #n = len(br.__data__)
 #dat = br.__data__
 #print(dat)
