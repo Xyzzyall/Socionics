9ef2b568d2a4c8bcf9e37bd3d9e61aa5a922de48
xyzzy <xyzzyall@outlook.com>
2020-02-22 18:07:52 +0300

    crawled some more.

--- MySocioCrawler.py
+++ MySocioCrawler.py
@@ -3,12 +3,12 @@ import random
 
 max_ind = 4009200
 
-indexes = list(range(3, max_ind, 1000))
+indexes = list(range(7, max_ind, 1000))
 
 f = open('my_socio_data.txt', 'w+')
 
 i = 0
-for data in MySocioParser.mysocio_crawler(indexes, sleep=1):
+for data in MySocioParser.mysocio_crawler(indexes, sleep=0.1):
     i += 1
     if i%10 == 0:
         print('loaded ' + str(i) + ' results.')
