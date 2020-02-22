ab49a2881a7679c2b0f1e8667020ece754d3b334
xyzzy <xyzzyall@outlook.com>
2020-02-22 23:47:00 +0300

    Some another MySocio results downloaded.

--- MySocioCrawler.py
+++ MySocioCrawler.py
@@ -3,15 +3,19 @@ import random
 
 max_ind = 4009200
 
-indexes = list(range(7, max_ind, 1000))
+indexes = list(range(3500000, max_ind, 100))
 
-f = open('my_socio_data.txt', 'w+')
+f = open('my_socio_data.txt', 'a')
 
 i = 0
-for data in MySocioParser.mysocio_crawler(indexes, sleep=0.1):
-    i += 1
-    if i%10 == 0:
-        print('loaded ' + str(i) + ' results.')
-    f.write(data + '\n')
+for data in MySocioParser.mysocio_crawler(indexes, sleep=0.3):
+    try:
+        i += 1
+        if i % 10 == 0:
+            print('loaded ' + str(i) + ' results.')
+        f.write('\n' + data)
+    except:
+        f.close()
+        break
 
 f.close()
