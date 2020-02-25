feea81105aa78e3985769399c7a6e190426f0eb3
Xyzzyall <xyzzyall@gmail.com>
2020-02-26 00:51:27 +0300

    enhanced socionics lib

--- Socionics/Socionics.py
+++ Socionics/Socionics.py
@@ -7,6 +7,7 @@ class Socionics:
 
     def __init__(self, data):
         self.__data__ = np.array(data)
+        self.name = 'unknown'
 
     @staticmethod
     def get_name(gr, i):
@@ -20,9 +21,24 @@ class Socionics:
     def get(name):
         pass
 
+    @staticmethod
+    def get_zero():
+        pass
+
     def mult(self, other):
         res = Socionics(np.matmul(self.__data__, np.transpose(other.__data__)))
         res.name = self.name + '*' + other.name + "'"
+        return res
+
+    def decompose(self):
+        """
+            ВАЖНО: считается, что все компоненты матрицы данных ровно распределены по базисам социотипа/отношений
+        :return:
+            процент "содержания" каждого социотипа/отношения в объекте
+        """
+        res = {}
+        for name in self.get_names():
+            res[name] = max(max(self.get(name) * self.__data__))
         return res
 
     def __eq__(self, other):
