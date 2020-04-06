ae8b6190c602e26b7b536369c968f962177f1821
Xyzzyall <xyzzyall@gmail.com>
2020-04-06 23:57:45 +0300

    Delete __init__.py

--- Good_4x4_Comp/__init__.py
+++
@@ -1,5 +0,0 @@
-from Good_4x4_Comp.CompToFour import CompToFour
-from Good_4x4_Comp.CompToTwo import CompToTwo
-from Good_4x4_Comp.Ptypes import Ptype
-from Good_4x4_Comp.SocMatrix import SMatrix
-from Good_4x4_Comp.Rtypes import Rtype
