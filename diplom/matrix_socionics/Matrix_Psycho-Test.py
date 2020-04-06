ca1ee85b3872960cb07ae409185e02cd722127b5
Alexandr <krivovalex4@yandex.ru>
2020-04-07 02:49:14 +0300

    Битва за оптимизацию, логику и комплексность

--- diplom/matrix_socionics/Matrix_Psycho-Test.py
+++ diplom/matrix_socionics/Matrix_Psycho-Test.py
@@ -1,13 +1,26 @@
 import diplom.matrix_socionics.SMatrix as PsyMtx
-
+from itertools import permutations
+import numpy as np
 
 psy = PsyMtx.Ptype.get_names()
 rel = PsyMtx.Rtype.get_names()
 
-for i in range(16):
-    a = PsyMtx.Ptype.get(psy[0]).mult(PsyMtx.Ptype.get(psy[i]))
-    print(a.name, rel[i])
-    print(a.__data__)
-
 print(PsyMtx.Ptype.get('Gu').mult(PsyMtx.Ptype.get('Gm')).__data__)
 print(PsyMtx.Rtype.get('RO').__data__)
+
+
+z = np.zeros((2, 2), dtype=np.complex)
+compare = np.array((2, 2), bool)
+l = 0
+for j in range(16):
+    k = 0
+    l = l + 1
+    print('Type', l, psy[l-1])
+    for i in permutations([0, 1, 2, 3]):
+        k += 1
+        i = list(i)
+        mtx = PsyMtx.Ptype.comp_to_two(j, i)
+        compare = mtx == z
+        if not compare.all():
+            print(k, i)
+            print(mtx)
